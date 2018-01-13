23504cfa3c8cb081ce2a1404a7031a2b7c5ee753
Banee Ishaque K <baneeishaque@gmail.com>
2018-01-13 21:00:54 +0530

    In

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+
+// Firebase API Key
+define('FIREBASE_API_KEY', 'AAAAmvb4d68:APA91bHsHLEKvy3fOHjBTMeF91WAt1esq4wnnqWw9vhwPogQ7W8kdHWfm610gxlHPDG4uzqCR3dLrQM7ocUnU7cXaZ1H8AoufWm1KLye0G0h0Qn11NBenjPmwdZsz0XSespC8pXxO3f8');
